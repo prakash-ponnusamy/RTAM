8679683d84feb514f1e9bf577cc18c2e90273ad6
prakash-ponnusamy <smartprakash@gmail.com>
2017-09-30 11:50:11 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+    <div>
+       
+        <div class='container'>
+<div class="header-content">
+    <div class="header-content-inner">
+        <div class="row">
+            <h1 id="homeHeading" style='color:white;'>Risk Appetite</h1> <hr>
+        </div>
+    </div>
+</div>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+     `
+})
+export class AppComponent {
+    pageTitle: string = 'Risk Appetite';
+}
